10477818385da025715162b7eaa7a80eca2deeee
monalitav <monalita001@gmail.com>
2025-08-05 17:56:50 +0700

    membuat gun dan gun type

---
+++ app/Filament/Resources/GunResource/Pages/CreateGun.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\GunResource\Pages;
+
+use App\Filament\Resources\GunResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateGun extends CreateRecord
+{
+    protected static string $resource = GunResource::class;
+}
--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Owner extends Pivot
+class Owner extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'address', 'phone'];
 }
