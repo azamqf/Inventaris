cec9df1c18cc72a0066a100f4c572d57ba6807cf
monalitav <monalita001@gmail.com>
2025-08-26 13:24:46 +0700

    membuat widget guns

--- app/Filament/Resources/GunResource/Pages/ListGuns.php
+++ app/Filament/Resources/GunResource/Pages/ListGuns.php
@@ -3,17 +3,26 @@
 namespace App\Filament\Resources\GunResource\Pages;
 
 use App\Filament\Resources\GunResource;
-use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\GunResource\Widgets\GunOverview; // panggil widget
 
 class ListGuns extends ListRecords
 {
     protected static string $resource = GunResource::class;
 
-    protected function getHeaderActions(): array
+    // Tombol Create otomatis muncul di Filament 3.x, bisa dihapus getHeaderActions jika tidak perlu
+    // Tapi kalau mau manual:
+    // protected function getHeaderActions(): array
+    // {
+    //     return [
+    //         \Filament\Actions\CreateAction::make(),
+    //     ];
+    // }
+
+    protected function getHeaderWidgets(): array
     {
         return [
-            Actions\CreateAction::make(),
+            GunOverview::class,
         ];
     }
 }
