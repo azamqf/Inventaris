c0b6dd8bb795fabf2ef0c24bfacf7a45de269977
monalitav <monalita001@gmail.com>
2025-08-26 13:50:06 +0700

    membuat widget gun

---
+++ database/migrations/create_guns_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('guns', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Nama senjata
+            $table->string('serial_number')->unique(); // Nomor seri unik
+            $table->foreignId('gun_type_id')->constrained('gun_types')->onDelete('cascade');
+            $table->foreignId('condition_id')->constrained('conditions')->onDelete('cascade');
+            $table->enum('status', ['available', 'unavailable'])->default('available');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('guns');
+    }
+};
